cef7daa77d66ccaadf28f575abaa231aadaa2043
Daoud-Hussain <daoudhussain302@gmail.com>
2023-12-24 16:37:27 +0500

    Creating trainer's card

--- index.js
+++ index.js
@@ -4,7 +4,8 @@
 
 import {AppRegistry} from 'react-native';
 import Home from './Home';
+import Trainers from './Trainers';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => Home);
+AppRegistry.registerComponent(appName, () => Trainers);
 
