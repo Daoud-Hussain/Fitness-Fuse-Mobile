6e6b4ae1d4e989590e9aa1e3e20feec4fe247a51
Daoud-Hussain <daoudhussain302@gmail.com>
2023-12-26 19:22:40 +0500

    Working on complete profile screen

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ import Trainers from './Trainers';
 import TrainersDetails from './TrainersDetails';
 import Workout from './Workout';
 import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => Workout);
+import AgeScreen from './Age';
+AppRegistry.registerComponent(appName, () => AgeScreen);
 
